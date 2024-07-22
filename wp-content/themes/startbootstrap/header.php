b89e75501f1cbd07cca887c7222a6efc79d7bc04
Jhonata Silva <cicerojhonataoficial@gmail.com>
2024-07-22 15:40:57 -0300

    perdi o banco de dados, projeto acabou aqui

---
+++ wp-content/themes/startbootstrap/header.php
@@ -0,0 +1,62 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>
+        Jhonata Dev - Portifólio - Freelancer
+    </title>
+    <link rel="canonical" href="http://www.torneseumprogramador.com.br">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.12.0/css/all.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/styles.css">
+    <meta name="ir-site-verification-token" value="-28223945">
+
+    <meta property="og:title" content="Freelancer - Theme Preview">
+    <meta property="og:site_name" content="Start Bootstrap">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="<?php echo get_stylesheet_directory_uri() ?>/assets/img/branding/og-start-bootstrap.png">
+    <meta property="og:url" content="https://startbootstrap.com/previews/freelancer/">
+    <meta property="og:image:alt" content="Freelancer - Theme Preview">
+
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:image" content="<?php echo get_stylesheet_directory_uri() ?>/assets/img/branding/start-bootstrap-logo-500x500.png">
+    <meta name="twitter:site" content="@SBootstrap">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/safari-pinned-tab.svg" color="#dd3d31">
+    <meta name="msapplication-TileColor" content="#dd3d31">
+    <meta name="theme-color" content="#ffffff">
+    <script type="text/javascript" src="//m.servedby-buysellads.com/monetization.js"></script>
+    <meta name="description" content="Tema criado por aluno torneseumprogramador" />
+    <meta name="author" content="TorneseUmprogramador" />
+    <title>Freelancer - Start Bootstrap Theme</title>
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="#page-top">Jhonatan Silva</a>
+            <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">Sobre</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contato</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ wp-content/themes/startbootstrap/page.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Tema construido no Desafio21Dias
+ *
+ * @link http://torneseumprogramador.com.br
+ *
+ * @package WordPress
+ * @subpackage Desafio21Dias
+ * @since Desafio21Dias
+ */
+
+get_header();
+
+$posts_per_page = isset($_POST["per_page"]) ? $_POST["per_page"] : 2;
+$url = get_stylesheet_directory_uri();
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$args = array(
+    'posts_per_page' => $posts_per_page,
+    'paged'  => $paged,
+    'post_type'     => 'post'
+);
+$the_query_post = new WP_Query($args);
+if ($the_query_post->have_posts()) {  ?>
+    <div class="container" style="margin-top: 150px;">
+        <h2>Habilidades</h2>
+        <p>Itens por páginas: <?php echo $posts_per_page; ?></p>
+        <?php if ($paged <= 1) { ?>
+            <form name="frmper_page" action="" method="post">
+                <select class="custom-select d-block w-20" name="per_page" id="per_page" placeholder="Itens por pagina..." required="">
+                    <option value="2">Escolha</option>
+                    <option value="2">2</option>
+                    <option value="4">4</option>
+                    <option value="10">10</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                    <option value="500">500</option>
+                </select>
+            </form>
+        <?php } ?>
+        <hr />
+        <p>Digite algo no campo abaixo para pesquisar títulos ou conteúdos na página corrente:</p>
+        <input class="form-control" id="txtbusca" type="text" placeholder="Pesquisa...">
+        <br>
+        <table class="table" id="tabHabilidades">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Imagem</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Conteúdo</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                while ($the_query_post->have_posts()) {
+                    $the_query_post->the_post();
+                ?>
+                    <tr>
+                        <th scope="row"><?php the_id(); ?></th>
+                        <td>
+                            <a href="<?php echo get_permalink(); ?>">
+                                <div style="width:128px; height: 128px; overflow: hidden;">
+                                    <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid', 'style' => 'width:100%')); ?>
+                                </div>
+                            </a>
+                        </td>
+                        <td><a href="<?php echo get_permalink(); ?>">
+                                <?php the_title(); ?>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="<?php echo get_permalink(); ?>">
+                                <?php the_content(); ?>
+                            </a>
+                        </td>
+                    </tr>
+                <?php
+
+                } // end while
+                ?>
+
+            </tbody>
+            <caption>
+                <?php
+                global $wp_query;
+                $wp_query = $the_query_post;
+                echo paginate_links(array(
+                    'prev_text' => __('<i class="fa fa-reply-all" aria-hidden="true"></i>'),
+                    'next_text' => __('<i class="fa fa-share" aria-hidden="true"></i>'),
+                    'before_page_number' => ' [ ',
+                    'after_page_number'  => ' ] ',
+                ));
+                ?>
+            </caption>
+        </table>
+    </div>
+<?php
+} //
+else { ?>
+    <div class="container" style="margin-top: 150px;">
+        <div style="text-align:right;cursor: pointer;" onclick="javascript:window.history.go(-1);">
+            <i class="fa fa-reply-all" aria-hidden="true"></i> Voltar
+        </div>
+    </div>
+    <hr />
+    <div style="margin-top: 150px;" class="text-center mt-4 alert alert-danger" role="alert">
+        <p>Nenhum post encontrado!</p>
+    </div>
+<?php
+} //end if
+?>
+</div>
+</div>
+</section>
+
+<?php get_footer(); ?>
